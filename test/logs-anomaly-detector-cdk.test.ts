c6c985315310d849520332820fd1b9e4f4276b3f
Mohd Yousuf Khan <khanyousuf6367@gmail.com>
2026-06-13 15:01:41 +0530

    Initial commit

---
+++ test/logs-anomaly-detector-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LogsAnomalyDetectorCdk from '../lib/logs-anomaly-detector-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/logs-anomaly-detector-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LogsAnomalyDetectorCdk.LogsAnomalyDetectorCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
